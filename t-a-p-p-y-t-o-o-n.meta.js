d3b28850f20f8199f917b806b4b1fd4887967338
kawanoryo1234 <kawanoryo1234@github.com>
2024-12-13 21:06:33 +0700

    init

---
+++ t-a-p-p-y-t-o-o-n.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         t-a-p-p-y-t-o-o-n
+// @namespace    https://github.com/kawanoryo1234/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.user.js
+// @version      2024.12.13
+// @description  down t-a-p-p-y-t-o-o-n
+// @author       You
+// @match        https://www.tappytoon.com/en/chapters/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.tappytoon.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @grant        none
+// @downloadURL  https://github.com/kawanoryo1234/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.user.js
+// @updateURL    https://github.com/kawanoryo1234/ngoai_sau_muoi/raw/main/t-a-p-p-y-t-o-o-n.meta.js
+// ==/UserScript==
